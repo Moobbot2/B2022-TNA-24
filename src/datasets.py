a64cda1422afa2df4cb63e796afe77ee1f368955
Ngô ĐứcTâm <133946215+Moobbot2@users.noreply.github.com>
2024-03-13 10:53:56 +0700

    udpate connect database

--- src/datasets.py
+++ src/datasets.py
@@ -1,8 +1,28 @@
 import pandas as pd
-from config import FEATURES, KQ, OUTPUT_LINK
-from connect_sql_database import mydb
+import program_database
+from config import FEATURES, KQ, OUTPUT_LINK, TABLE_NAME
 
-df = pd.read_excel(OUTPUT_LINK)
+# Connect to the database
+mydb = program_database.connect_to_database()
 
-X = df[FEATURES]
-Y = df[KQ]
+# Check if connection was successful
+if mydb:
+    # Fetch data from the table
+    data_ungthu = program_database.fetch_data_from_table(mydb, TABLE_NAME)
+
+    # Check if data fetching was successful
+    if data_ungthu:
+        # Create DataFrame
+        column_names = FEATURES + [KQ]  # Replace with actual column names
+        df = pd.DataFrame(data_ungthu, columns=column_names)
+
+        # Access features and target variable
+        X = df[FEATURES]
+        Y = df[KQ]
+
+        # Close database connection
+        mydb.close()
+    else:
+        print("Failed to fetch data from the database.")
+else:
+    print("Failed to connect to the database.")
