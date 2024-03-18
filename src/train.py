baa5cd55253d037fab9c54a077a1f0288868b5c9
Tam Ngo <ngotamcv01@gmail.com>
2024-03-18 20:27:46 +0700

    update README, requirements

--- src/train.py
+++ src/train.py
@@ -6,11 +6,11 @@ program.train_evaluate_visualize_decision_tree(
     X, Y, classifier_type='DecisionTree')
 print('----- ----- -----')
 
-# print('Train RandomForest')
-# program.train_evaluate_visualize_decision_tree(
-#     X, Y, classifier_type='RandomForest', save_model_path='output/model/random_forest_model.joblib')
-# print('----- ----- -----')
+print('Train RandomForest')
+program.train_evaluate_visualize_decision_tree(
+    X, Y, classifier_type='RandomForest')
+print('----- ----- -----')
 
-# print('Train XGBoost')
-# program.train_evaluate_visualize_decision_tree(
-#     X, Y, classifier_type='XGBoost', save_model_path='output/model/xgboost_model.joblib')
+print('Train XGBoost')
+program.train_evaluate_visualize_decision_tree(
+    X, Y, classifier_type='XGBoost')
